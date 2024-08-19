9478fc9acfb523d59bb2ee0bdb741d9ed5529009
guowei <112166729+dgw2030521@users.noreply.github.com>
2024-08-19 17:55:55 +0800

    chore: init

--- postcss.config.js
+++ postcss.config.js
@@ -6,7 +6,7 @@ module.exports = {
     'postcss-write-svg': {
       utf8: false,
     },
-    'postcss-px-to-viewport': {
+    'postcss-px-to-viewport-8-plugin': {
       unitToConvert: 'px', // 要转化的单位
       viewportWidth: 1920, // UI设计稿的宽度
       unitPrecision: 5, // 转换后的精度，即小数点位数
