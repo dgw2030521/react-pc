ec74d16def68c6c659ed6d9a9aa481263dd1095e
guowei.dong <526470285@qq.com>
2024-11-05 22:47:00 +0800

    feat: changes

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -3,4 +3,11 @@ const prettierConfigStandard = require('prettier-config-standard');
 module.exports = {
   ...prettierConfigStandard,
   jsxSingleQuote: true,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 80,
+  proseWrap: 'never',
+  arrowParens: 'avoid',
 };
